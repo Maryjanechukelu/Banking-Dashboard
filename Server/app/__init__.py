b6b5d03bfc715f8521abfd3e646fe9a87effacdb
attamakevin <kevinejikez@gmail.com>
2024-08-12 21:22:04 +0100

    implemented CORS

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS  # Import flask-cors
 import datetime
 
 db = SQLAlchemy()
@@ -15,15 +16,19 @@ blacklist = set()
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
-    
+
     db.init_app(app)
     migrate.init_app(app, db)
     bcrypt.init_app(app)
     jwt.init_app(app)
-    
+
+    # Register blueprints
     from app.auth.views import auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
-    
+
+    # Enable CORS for the entire application
+    CORS(app, resources={r"/*": {"origins": "*"}})
+
     @jwt.token_in_blocklist_loader
     def check_if_token_is_revoked(jwt_header, jwt_payload):
         jti = jwt_payload['jti']
